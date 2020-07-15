da39fb7ce18cfff8c7806986ca9d31f0e45ef2a3
BID90 <bradea.ioan.daniel@gmail.com>
2020-07-15 22:33:19 +0300

    config

---
+++ build/precache-manifest.467c26caedd659605dec9d5742927e7d.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8ab89402115fa6f177c8533a21e92db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6934d4b97a57a3f1d065",
+    "url": "/static/css/2.9ff4381a.chunk.css"
+  },
+  {
+    "revision": "d43b565a710b554e7336",
+    "url": "/static/css/main.975f9863.chunk.css"
+  },
+  {
+    "revision": "6934d4b97a57a3f1d065",
+    "url": "/static/js/2.aac67916.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.aac67916.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d43b565a710b554e7336",
+    "url": "/static/js/main.4e9b3182.chunk.js"
+  },
+  {
+    "revision": "dc0cbb72e1925d219165",
+    "url": "/static/js/runtime-main.718cb260.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/static/media/notification.c0d3c94c.eot"
+  }
+]);
--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,3 @@
 export const config = {
-    apiUrl : "http://app-bid90-be",
+    apiUrl : "app-bid90-be",
 }
